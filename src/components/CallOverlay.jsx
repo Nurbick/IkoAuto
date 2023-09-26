0ab5d5b33ecfdd8be0ca3b2d828120238515c690
Nurbol Esenbekov <nurbolesenbekov6@gmail.com>
2023-09-25 22:24:19 -0700

    l

--- src/components/CallOverlay.jsx
+++ src/components/CallOverlay.jsx
@@ -9,6 +9,7 @@ const CallOverlay = () => {
                     <img src={callOverlay} alt="" className="callOverlay__img" />
                 </div>
                 <div className="callOverlay__right">
+                <div className="callOveray__exit">X</div>
                     <div className="callOverlay__form">
                         <h1 className="callOverlay__title">Заказать обратный звонок</h1>
                         <p className="callOverlay__desc">Отправьте нам свой номер телефона и мы Вам позвоним!</p>
@@ -16,7 +17,7 @@ const CallOverlay = () => {
                         <input placeholder='Телефон' className='callOverlay__input' type="text" />
                         <div className="callOverlay__select">
                             <input type="checkbox" className="callOverlay__checkbox" />
-                            <span className='callOverlay__select-desc'></span>
+                            <span className='callOverlay__select-desc'>Я принимаю пользовательское соглашение</span>
                         </div>
                         <button className="callOverlay__btn">Отправить</button>
                     </div>
