13d62c109f2afca8804eee2a4cd59a08eb60e61d
Nurbol Esenbekov <nurbolesenbekov6@gmail.com>
2023-09-25 19:09:39 -0700

    forst

---
+++ src/components/CallOverlay.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const CallOverlay = () => {
+    return (
+        <div className='callOverlay'>
+            <div className='callOverlay__content'>
+                <div className="callOverlay__left"></div>
+                <div className="callOverlay__right">
+                    <div className="callOverlay__form">
+                        <h1 className="callOverlay__title">Заказать обратный звонок</h1>
+                        <p className="callOverlay__desc">Отправьте нам свой номер телефона и мы Вам позвоним!</p>
+                        <input placeholder='Имя' className='callOverlay__input' type="text" />
+                        <input placeholder='Телефон' className='callOverlay__input' type="text" />
+                        <div className="callOverlay__select">
+                            <input type="checkbox" className="callOverlay__checkbox" />
+                            <span className='callOverlay__select-desc'></span>
+                        </div>
+                        <button className="callOverlay__btn">Отправить</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CallOverlay
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Start from "./Start/Start";
 import Examples from "./Examples/Examples";
+import CallOverlay from '../../components/CallOverlay';
 
 const Home = () => {
     return (
         <main className="home">
+            <CallOverlay/>
             <Start/>
             <Examples/>
         </main>
